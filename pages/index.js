8aac79e7d4e9cfa49c05bf766c2bdc93d3b062fb
in8ly <urok123@gmail.com>
2025-02-12 12:24:51 -0800

    *Back to the start once more after many endings*

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BreathingAnimation from '../components/BreathingAnimation';
+
+export default function Home() {
+    return (
+        <main className="min-h-screen">
+            <BreathingAnimation />
+        </main> 
+    );
+)
